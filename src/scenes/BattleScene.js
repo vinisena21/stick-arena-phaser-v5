98b2eb5bcf95183b2f769660bb8b5ccff4f0f416
vinisena21 <viniciussena901@gmail.com>
2026-05-12 21:30:38 -0300

    Update BattleScene.js

--- src/scenes/BattleScene.js
+++ src/scenes/BattleScene.js
@@ -1,26 +1,58 @@
 import Phaser from "phaser";
+import MobileControls from "../systems/MobileControls.js";
 
-export default class BootScene extends Phaser.Scene {
+export default class BattleScene extends Phaser.Scene {
   constructor() {
-    super("BootScene");
+    super("BattleScene");
   }
 
-  preload() {
-    this.load.setPath("./");
+  init(data) {
+    this.character = data.character;
+    this.map = data.map;
+    this.weapon = data.weapon;
   }
 
   create() {
-    this.cameras.main.setBackgroundColor("#020617");
+    const w = this.scale.width;
+    const h = this.scale.height;
 
-    this.add.text(this.scale.width / 2, this.scale.height / 2, "STICK ARENA V5", {
+    this.add.rectangle(w / 2, h / 2, w, h, this.map.background || 0x020617);
+
+    this.add.text(w / 2, 30, this.map.name, {
       fontFamily: "Arial",
-      fontSize: 42,
-      fontStyle: "900",
-      color: "#67e8f9",
+      fontSize: 28,
+      color: "#ffffff",
     }).setOrigin(0.5);
 
-    this.time.delayedCall(1000, () => {
-      this.scene.start("MenuScene");
-    });
+    this.player = this.add.rectangle(180, h - 180, 60, 120, this.character.color);
+    this.enemy = this.add.rectangle(w - 180, h - 180, 60, 120, 0xef4444);
+
+    this.physics.add.existing(this.player);
+    this.physics.add.existing(this.enemy);
+
+    this.player.body.setCollideWorldBounds(true);
+    this.enemy.body.setCollideWorldBounds(true);
+
+    this.controls = new MobileControls(this);
+
+    this.cameras.main.fadeIn(400);
+  }
+
+  update() {
+    const state = this.controls.state;
+
+    this.player.body.setVelocityX(0);
+
+    if (state.left) {
+      this.player.body.setVelocityX(-320);
+    }
+
+    if (state.right) {
+      this.player.body.setVelocityX(320);
+    }
+
+    if (state.jump && this.player.body.blocked.down) {
+      this.player.body.setVelocityY(-600);
+    }
   }
 }
